6d693bf2526a66a1bb70bee7145601b45081a263
Katherine Ren <kr5379@princeton.edu>
2026-03-30 23:02:33 -0400

    missed one

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from config import DATABASE_URL
+from backend.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
