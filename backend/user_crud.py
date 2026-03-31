f4bf315bd753ade1540cecce09b767ecf1f3c34a
Katherine Ren <kr5379@princeton.edu>
2026-03-31 15:03:33 -0400

    update on user_crud

--- backend/user_crud.py
+++ backend/user_crud.py
@@ -3,13 +3,11 @@ from sqlalchemy.orm import Session
 from uuid import UUID
 from backend.models import User
 
-def get_or_create_user(db: Session, user_id: UUID) -> User:
-    user = db.query(User).filter(User.id == user_id).first()
-    if user:
-        return user
+from sqlalchemy.dialects.postgresql import insert as pg_insert
 
-    user = User(id=user_id)
-    db.add(user)
+def get_or_create_user(db: Session, user_id: UUID) -> User:
+    stmt = pg_insert(User).values(id=user_id).on_conflict_do_nothing(index_elements=["id"])
+    db.execute(stmt)
     db.commit()
-    db.refresh(user)
-    return user
+    
+    return db.query(User).filter(User.id == user_id).first()
